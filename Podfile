2ce2f9888340c6eff4c481a9f7314040227e37cd
Elissa Perdue <elissaperdue@gmail.com>
2020-07-31 22:23:15 -0400

    first commit :)

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '10.0'
+
+target 'nav' do
+  use_frameworks!
+
+pod 'RadarSDK', '~> 3.0.0'
+
+
+end
---
+++ nav/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  nav
+//
+//  Created by Elissa Perdue on 7/18/20.
+//  Copyright © 2020 Elissa Perdue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var starteverything:UIButton!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+        if starteverything.pressed
+    
+    }
+
+
+}
+
